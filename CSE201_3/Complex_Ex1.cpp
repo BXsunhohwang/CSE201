1234151c35758d7933c38bf5ac52b7e6741c08c2
sunho.hwang <sunho.hwang@polytechnique.edu>
2019-10-10 11:14:58 +0200

    first

--- CSE201_3/Complex_Ex1.cpp
+++ CSE201_3/Complex_Ex1.cpp
@@ -5,8 +5,26 @@ private:
 
   public:
     // construct the complex number zero
-    Complex();
+    Complex() { 
+        real = 0;
+        imaginary = 0;
+    }
 
     // construct a complex number with given real and imaginary parts
-    Complex(double real, double imaginary);
+    Complex(double real, double imaginary) {
+        (*this).real = real;
+
+    }
+
+
+    ~Complex(){ std::cout << "Destroying number " << real << " + " << imaginary << "*i" << std::endl; }
 };
+
+int main () {
+    Complex c();
+    Complex d(3,0);
+    Complex e(1,2);
+    return 0;
+
+
+}
