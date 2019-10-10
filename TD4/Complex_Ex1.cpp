90192732013ecb86a6021f4772653305e65ab201
sunho.hwang <sunho.hwang@polytechnique.edu>
2019-10-10 12:19:10 +0200

    second

--- TD4/Complex_Ex1.cpp
+++ TD4/Complex_Ex1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 
 
@@ -21,15 +22,51 @@ private:
 
     }
 
+    double getReal() { return (*this).real;}             // get the real part of the number
+    double getImaginary() {return (*this).imaginary;}        // get the imaginary part of the number
+    void setReal(double re) {real = re;}      // set the value of the real part
+    void setImaginary(double im) {imaginary = im;} // set the value of the imaginary part
+
+
+    double magnitude() {
+      double x = pow ((*this).real,2);
+      double y = pow((*this).imaginary, 2);
+      double nbr = x + y;
+      double mag =sqrt(nbr);
+      return mag;
+        }           // computes the distance from the complex origin
+
+      /*
+        modifies the number so its magnitude becomes 1
+        but it stays in the same direction from the complex origin, if possible. In other
+      words, project the original number onto the unit circle. If that operation is not possible, the complex number must be left unchanged.
+      */
+    void normalize() {
+      double mag = magnitude();
+      imaginary = getImaginary()/mag;
+      real = getReal() / mag;
+
+    }
 
     ~Complex(){ std::cout << "Destroying number " << real << " + " << imaginary << "*i" << std::endl; }
+
 };
 
-int main () {
-    Complex c(0,0);
-    Complex d(3,0);
-    Complex e(1,2);
-    return 0;
 
 
+int main() {
+  Complex c1;
+  Complex c2 = Complex(3,4);
+
+  c1.normalize();
+
+  c1.setReal(c2.getReal());
+  c1.setImaginary(c2.getImaginary());
+
+  std::cout << "c1 magnitude: " << c1.magnitude() << std::endl;
+  c1.normalize();
+
+  std::cout << "c1 is: " << c1.getReal() <<
+    " + " << c1.getImaginary() << " * i" << std::endl;
+  std::cout << "c1 magnitude:" << c1.magnitude() << std::endl;
 }
